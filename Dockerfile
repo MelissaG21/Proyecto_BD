2b1db4398accaefb8cb4fe7a57298cdd45cba7dc
Ximena Romero <a329898@uach.mx>
2021-05-25 08:46:53 -0600

    Modifique formularios

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM node:14
-WORKDIR /app
-COPY package.json ./
-ADD src ./
-RUN npm install
-COPY . .
-CMD ["node","src/app.js"]
--- WWW/conexion2.php
+++ WWW/conexion2.php
@@ -8,26 +8,19 @@
     if(!$enlace){
         echo"Error en la conexion con el servidor";
     }
-
-    $vehicle_rental_id =$_POST["vehicle_rental_id"];
-    $date_from=$_POST["date_from"];
-    $date_to=$_POST["date_to"];
-    $reg_number=$_POST["reg_number"];
     $customer_id=$_POST["customer_id"];
-    $rental_status_code='0';
+    $customer_name = $_POST["customer_name"];
+    $customer_details = $_POST["customer_details"];
 
-    $insertarDatos2 = "INSERT INTO vehicle_rentals VALUES('$vehicle_rental_id',
-                                                        '$date_from',
-                                                        '$date_to',
-                                                        '$reg_number',
-                                                        '$customer_id',
-                                                        '$rental_status_code')";
-    $ejecutarInsertar2 = mysqli_query($enlace,$insertarDatos2);
+    $insertarDatos="INSERT INTO customers VALUES('$customer_id',
+                                                    '$customer_name',
+                                                    '$customer_details')";
+        
+    $ejecutarInsertar = mysqli_query($enlace,$insertarDatos);
 
-    if(!$ejecutarInsertar2){
-        echo"Error En  linea sql";
-    }
-
+    if(!$ejecutarInsertar){
+            echo"Error En  linea sql";
+        }
 ?>
 <html lang="en">
 <head>
